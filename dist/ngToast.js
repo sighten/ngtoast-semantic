c0fde5eaf44fc64493c1a581c406ef3b4c168815
Todd Bracken <todd@sighten.io>
2016-07-08 14:06:15 -0700

    fix js build

--- dist/ngToast.js
+++ dist/ngToast.js
@@ -299,15 +299,3 @@
     ]);
 
 })(window, window.angular);
-
-(function(window, angular) {
-  'use strict';
-
-  angular
-    .module('ngToast', [
-      'ngSanitize',
-      'ngToast.directives',
-      'ngToast.provider'
-    ]);
-
-})(window, window.angular);
